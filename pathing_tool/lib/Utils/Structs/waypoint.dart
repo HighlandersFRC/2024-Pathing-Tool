bdcfa67dbb51e952f2f90976a32eb55b4a52af88
Deepesh Kothari <112287069+Deepestest@users.noreply.github.com>
2025-09-13 14:58:48 -0600

    fixed a bug with exporting nan numbers

--- pathing_tool/lib/Utils/Structs/waypoint.dart
+++ pathing_tool/lib/Utils/Structs/waypoint.dart
@@ -59,17 +59,22 @@ class Waypoint {
   }
 
   Map<String, dynamic> toJson() {
+    double sanitize(double value) {
+      if (value.isNaN || value.isInfinite) return 0.0;
+      return value;
+    }
+
     return {
-      "x": x,
-      "y": y,
-      "angle": theta,
-      "x_velocity": dx,
-      "y_velocity": dy,
-      "angular_velocity": dtheta,
-      "x_acceleration": d2x,
-      "y_acceleration": d2y,
-      "angular_acceleration": d2theta,
-      "time": t
+      "x": sanitize(x),
+      "y": sanitize(y),
+      "angle": sanitize(theta),
+      "x_velocity": sanitize(dx),
+      "y_velocity": sanitize(dy),
+      "angular_velocity": sanitize(dtheta),
+      "x_acceleration": sanitize(d2x),
+      "y_acceleration": sanitize(d2y),
+      "angular_acceleration": sanitize(d2theta),
+      "time": sanitize(t)
     };
   }
 
